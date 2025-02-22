3d450cd19498dbc8eddd5e9c7d9e3f1040546aef
jaykmarBCET <jaykumar3dear@gmail.com>
2025-02-23 00:06:34 +0530

    updated

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -4,7 +4,7 @@ export default function Footer() {
     
     return (
         <>
-            <div className="container mx-auto mt-10 w-full">
+            <div className="container mx-auto mt-10 vb w-full">
                 <footer className="py-4 flex flex-col ">
                     <ul className="flex justify-center border-b py-4 rounded-lg bg-gray-200 border-gray-300 pb-3 mb-3">
                         <li className="nav-item mx-2"><NavLink to="/" >Home</NavLink></li>
